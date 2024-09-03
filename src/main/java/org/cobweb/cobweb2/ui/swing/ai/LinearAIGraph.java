0df484e6f1034c6b8f0b5b382cc57dd7d427dbc4
cpap7 <mjajarmi2000@outlook.com>
2024-09-03 01:05:40 -0400

    Updated LinearAIGraph.java. Removed Swing/AWT dependencies and replaced it with JavaFX.

--- src/main/java/org/cobweb/cobweb2/ui/swing/ai/LinearAIGraph.java
+++ src/main/java/org/cobweb/cobweb2/ui/swing/ai/LinearAIGraph.java
@@ -1,5 +1,111 @@
 package org.cobweb.cobweb2.ui.swing.ai;
 
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.util.Duration;
+
+import org.cobweb.cobweb2.impl.ai.LinearWeightsControllerParams;
+
+/**
+ * Creates a bar chart containing AI data
+ * */
+public class LinearAIGraph extends Stage {
+	// TODO: Implement serializable interface
+	private static final long serialVersionUID = 674068154319803208L;
+
+	private final Timeline refreshTimer; 				   // Needed for refreshing the data on the graphs
+
+	private final LinearWeightsControllerParams LWCParams; // LWC parameters for graph data
+
+	// Global objects for the graphs
+	private final BarChart barChart;
+	private final XYChart.Series<String, Number> xySeriesChart = new XYChart.Series<>();
+
+
+	/** Constructor for the LinearAIGraph object
+	 * @param newControllerParams specified controller parameters
+	 * */
+	public LinearAIGraph(LinearWeightsControllerParams newControllerParams) {
+		// Initialize/set the LWC params
+		this.LWCParams = newControllerParams;
+
+		// Set the title of the stage (or window)
+		setTitle("AI Output Distribution");
+
+		// Create the y-axis
+		CategoryAxis y = new CategoryAxis();
+		y.setLabel("Value");
+
+		// Create the x-axis
+		CategoryAxis x = new CategoryAxis();
+		x.setLabel("Output");
+
+		// Initialize the bar chart object
+		this.barChart = new BarChart<>(x, y);
+		barChart.setTitle("AI Output Distribution"); // Set the title of the bar chart
+		barChart.getData().add(xySeriesChart); 		 // Add the x & y series chart to the bar chart
+
+		// Loop through each output name within the LWC
+		for(String names : LinearWeightsControllerParams.outputNames) {
+			// Add each data point with initial values of 0 to start
+			xySeriesChart.getData().add(new XYChart.Data<>(names, 0));
+		}
+
+		// Create the window for the graph
+		Scene scene = new Scene (barChart, 800, 800); // Scene contains the bar chart, displaying it at 800x800 resolution
+		setScene(scene); // Place the scene onto the stage
+
+		// Initialize the refresh timer using KeyFrame to trigger the refreshData() method every 100 ms
+		refreshTimer = new Timeline(new KeyFrame(Duration.millis(100), event -> refreshData()));
+
+		// Set the cycle count to ensure data will be refreshed every 100 ms
+		refreshTimer.setCycleCount(Timeline.INDEFINITE); // Cycle count is indefinite until the window is closed
+
+		// Refresh data when the window is shown
+		setOnShown(event -> {
+			refreshTimer.play();
+		});
+
+		// TODO: Test this and compare hardware resource consumption
+		// setOnShowing(event -> refreshTimer.play());
+
+		// Stop the refresh timer when the window is closed or minimized
+		setOnHidden(event -> {
+			refreshTimer.stop();
+		});
+
+		// TODO: Test these and compare hardware resource consumption
+		// setOnHiding(event -> refreshTimer.pause());
+		// setOnHiding(event -> refreshTimer.stop());
+		// setOnHidden(event -> refreshTimer.pause());
+
+		// Display the window
+		show(); // 'Stage' method to display that stage
+	}
+
+	/** Refreshes data being displayed on the chart */
+	private void refreshData() {
+		// Declare an array to retrieve and store the latest data from LWC parameters
+		double[] data = LWCParams.getRunningOutputMean();
+
+		// Handle UI updates
+		Platform.runLater(() -> { // Update the UI on the JavaFX application thread
+			// Loop through the data
+			for(int i = 0; i < LinearWeightsControllerParams.OUTPUT_COUNT; i++) {
+				// Update each Y value on the XY series chart
+				xySeriesChart.getData().get(i).setYValue(data[i]);
+			}
+		});
+	}
+}
+
+/* Deprecated code used for reference. Uses old Swing/AWT libraries
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
@@ -17,9 +123,6 @@ import org.jfree.data.category.DefaultCategoryDataset;
 
 public class LinearAIGraph extends JFrame implements WindowListener, ActionListener {
 
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = 674068154319803208L;
 
 	private ChartPanel chartPan;
@@ -82,10 +185,11 @@ public class LinearAIGraph extends JFrame implements WindowListener, ActionListe
 	public void windowOpened(WindowEvent e) {
 		refreshTimer.start();
 	}
-
+*/
 	/**
 	 * Refresh timer function
 	 */
+	/*
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		double data[] = params.getRunningOutputMean();
@@ -95,3 +199,4 @@ public class LinearAIGraph extends JFrame implements WindowListener, ActionListe
 	}
 
 }
+*/
