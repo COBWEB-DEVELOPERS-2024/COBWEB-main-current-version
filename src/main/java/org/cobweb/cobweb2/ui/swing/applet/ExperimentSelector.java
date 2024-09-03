e6b96c8fb54d5189f2f99b24cc579b521eb8eb4f
cpap7 <mjajarmi2000@outlook.com>
2024-09-03 02:49:38 -0400

    Removed Swing dependencies. Added JavaFX dependencies.

--- src/main/java/org/cobweb/cobweb2/ui/swing/applet/ExperimentSelector.java
+++ src/main/java/org/cobweb/cobweb2/ui/swing/applet/ExperimentSelector.java
@@ -3,6 +3,9 @@
  */
 package org.cobweb.cobweb2.ui.swing.applet;
 
+import javafx.collections.FXCollections;
+import javafx.scene.control.ComboBox;
+
 import java.util.Map;
 
 import javax.swing.JComboBox;
@@ -10,18 +13,25 @@ import javax.swing.JComboBox;
 /**
  *
  */
-public class ExperimentSelector extends JComboBox<String> {
-
+public class ExperimentSelector extends ComboBox<String> {
 	/**
 	 *
 	 */
 	private static final long serialVersionUID = 8328713697383538804L;
 
-	public ExperimentSelector(Map<String, String> experements) {
-		super(experements.keySet().toArray(new String[0]));
+	public ExperimentSelector(Map<String, String> experiments) {
+		// super(experiments.keySet().toArray(new String[0])); // OLD - for JComboBox (Swing)
+
+		// Initialize ComboBox with a list of experiment names
+		super(FXCollections.observableArrayList(experiments.keySet()));
+
+		// Disable editing for the ComboBox
 		this.setEditable(false);
 
-		this.setMaximumRowCount(5);
+		// Set the amount of visible rows (max = 5)
+		this.setVisibleRowCount(5); // 5 rows visible when the dropdown is shown
+
+		// this.setMaximumRowCount(5); // OLD - for JComboBox (Swing)
 
 	}
 
