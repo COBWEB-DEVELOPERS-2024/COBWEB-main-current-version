d687287352a777199b8d85d0008d635dff4d7ba0
cpap7 <mjajarmi2000@outlook.com>
2024-09-03 01:06:12 -0400

    Updated LinearAIViewer.java. Removed Swing/AWT dependencies and replaced it with JavaFX.

--- src/main/java/org/cobweb/cobweb2/ui/swing/ai/LinearAIViewer.java
+++ src/main/java/org/cobweb/cobweb2/ui/swing/ai/LinearAIViewer.java
@@ -1,12 +1,10 @@
 package org.cobweb.cobweb2.ui.swing.ai;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import javafx.stage.WindowEvent;
 
 import org.cobweb.cobweb2.impl.ai.LinearWeightsControllerParams;
 import org.cobweb.cobweb2.ui.ViewerClosedCallback;
 import org.cobweb.cobweb2.ui.ViewerPlugin;
-
 
 public class LinearAIViewer implements ViewerPlugin {
 
@@ -14,43 +12,59 @@ public class LinearAIViewer implements ViewerPlugin {
 	private ViewerClosedCallback onClosed;
 	private LinearWeightsControllerParams controllerParams;
 
+	/** Constructor for the LinearAIViewer object
+	 * @param controllerParams parameters for the AI controller */
 	public LinearAIViewer(LinearWeightsControllerParams controllerParams) {
 		this.controllerParams = controllerParams;
-
 	}
 
+	/** Method to handle window close requests and display the window containing the graph */
 	@Override
 	public void on() {
+		// Create the window
 		aiGraph = new LinearAIGraph(controllerParams);
-		aiGraph.setVisible(true);
-		aiGraph.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				onClosed.viewerClosed();
-			}
+
+		// Event handler for the window close request
+		aiGraph.setOnCloseRequest(event -> {
+			onClosed.viewerClosed(); // Viewer has been closed
 		});
+
+		// setVisible(true) // Old Swing method
+
+		// Display the window containing the graph
+		aiGraph.show();
 	}
 
+	/** Method to handle the window being closed and viewer being turned off */
 	@Override
 	public void off() {
-		if (aiGraph == null)
-			return;
-		aiGraph.setVisible(false);
-		aiGraph.setEnabled(false);
+		// Ensure that the window is created
+		if (aiGraph == null) { return; } // If it's not created, don't do anything
+
+		// Deprecated Swing stuff
+		// aiGraph.setVisible(false);
+		// aiGraph.setEnabled(false);
+
+		aiGraph.close(); // Close the graph
+
+		// Set the reference to the window to null
 		aiGraph = null;
 	}
 
+	/** Method to return the name of the viewer plugin
+	 * @return String containing the name of the viewer plugin */
 	@Override
 	public String getName() {
 		return "AI Weight Stats";
 	}
 
+	/** Method to set the callback to be called when AI graph viewer gets closed */
 	@Override
 	public void setClosedCallback(ViewerClosedCallback onClosed) {
 		this.onClosed = onClosed;
-
 	}
 
+	/** Method for ensuring the AI graph viewer is closed --> garbage collection */
 	@Override
 	public void dispose() {
 		off();
